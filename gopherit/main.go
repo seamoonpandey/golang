2171cc24fca0e2af3bb76cc71763bf42ac8d2f75
Seamoon Pandey <hackedasocial@gmail.com>
2024-09-13 14:25:25 +0545

    draw ascii art for gopher

--- gopherit/main.go
+++ gopherit/main.go
@@ -5,8 +5,16 @@ import (
 )
 
 func main(){
-    f.Println("  __      _")
-    f.Println("o'')}____//")
-    f.Println(" `_/      )")
-    f.Println(" (_(_/-(_/ ")
+    f.Println("    `.-::::::-.`    ")
+    f.Println(".:-::::::::::::::-:.")
+    f.Println("`_:::    ::    :::_`")
+    f.Println(" .:( ^   :: ^   ):. ")
+    f.Println(" `:::   (..)   :::. ")
+    f.Println(" `:::::::UU:::::::` ")
+    f.Println(" .::::::::::::::::. ")
+    f.Println(" O::::::::::::::::O ")
+    f.Println(" -::::::::::::::::- ")
+    f.Println(" `::::::::::::::::` ")
+    f.Println("  .::::::::::::::.  ")
+    f.Println("    oO:::::::Oo     ")
 }
